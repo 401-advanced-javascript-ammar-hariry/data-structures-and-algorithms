85bae6ab8a14314375a909e0d5dc88196715df00
Ammarhr <hariry49@gmail.com>
2020-05-20 06:10:12 +0300

    binary search challenge

---
+++ arrayBinarySearch/__test__/arrayBinarrySearch.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const binarySearch = require('../array-binary-search');
+
+describe('list', () => {
+
+    it('it should return -1 when the value not inside the array', () => {
+        let searchArr = [2, 5, 7, 3];
+        expect(binarySearch(searchArr, 4)).toEqual(-1);
+    });
+
+    it('inputs can be string also ', () => {
+        let searchArr = [5, 4, 9, 3, 10];
+        expect(binarySearch(searchArr, 10)).toEqual(1);
+    });
+
+
+
+});
